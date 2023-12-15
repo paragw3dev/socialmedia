8858d10a66bac30d147aeef91a7fee8e7d03233b
paragw3dev <152947223+paragw3dev@users.noreply.github.com>
2023-12-15 08:51:01 +0530

    seconddsd

--- server/index.js
+++ server/index.js
@@ -18,20 +18,20 @@ const CONNECTION_URL = "mongodb+srv://socialmedia:mediasocial@cluster2.kyj30ds.m
 const PORT = process.env.PORT || 5000;
 console.log("object");
 
-// mongoose.connect (CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-//     .catch((error) => console.log(error.message));
+mongoose.connect (CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
 
-    const startServer = async () => {
-        try {
-          await mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    // const startServer = async () => {
+    //     try {
+    //       await mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
       
-          app.listen(PORT, () => {
-            console.log(`Server running on port: ${PORT}`);
-          });
-        } catch (error) {
-          console.error('Error connecting to MongoDB:', error.message);
-        }
-      };
+    //       app.listen(PORT, () => {
+    //         console.log(`Server running on port: ${PORT}`);
+    //       });
+    //     } catch (error) {
+    //       console.error('Error connecting to MongoDB:', error.message);
+    //     }
+    //   };
       
-      startServer();
+    //   startServer();
